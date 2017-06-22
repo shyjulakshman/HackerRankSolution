18deb57892db51747874c85cca4ef032fa98c5df
shyjulakshman <shyjulakshman@gmail.com>
2017-06-23 00:12:26 +0530

    GridlandMetro updated

--- src/com/hackerrank/algorithm/search/GridlandMetro.java
+++ src/com/hackerrank/algorithm/search/GridlandMetro.java
@@ -34,10 +34,84 @@ public class GridlandMetro {
 			for(Track track:trackList){
 				int columnStart = track.columnStart;
 				int columnEnd= track.columnEnd;
+				count += (columnEnd-columnStart)+1;
 			}
 			
 		}		
 		return count;
+	}
+	
+	
+	
+	public static List<Track> getTrackList(List<Track> list, Track e){
+		
+		if(list.isEmpty()){
+			list.add(e);
+		}else{
+			int index=0;
+			while(index<list.size()){
+				Track cur= list.get(index);		
+				
+				// cur 3 4   e  4 5
+				if(e.columnStart==cur.columnEnd){
+					cur.columnEnd=e.columnEnd;
+					return list;
+				}
+				
+				// cur 3 4  e 1 3
+				if(e.columnEnd==cur.columnStart){
+					cur.columnStart=e.columnStart;
+					return list;
+				}
+				
+				
+				//if within range ignore
+				if(e.columnStart>=cur.columnStart && e.columnEnd<=cur.columnEnd){
+					return list;
+				}
+				
+			
+				if(e.columnStart<cur.columnStart && e.columnEnd==cur.columnEnd){
+					cur.columnStart=e.columnStart;
+					if(index>0){
+						Track prev= list.get(index-1);
+						if(prev.columnEnd==cur.columnStart){							
+							cur.columnStart= prev.columnStart;
+							list.remove(index-1);
+						}						
+					}
+					return list;
+				}
+				
+				if(e.columnStart==cur.columnStart &&  e.columnEnd>cur.columnEnd){
+					 cur.columnEnd=e.columnEnd;
+					 if(index<=list.size()-2){
+						 Track next= list.get(index+1);
+						 if(next.columnStart==cur.columnEnd){
+							 cur.columnEnd=next.columnEnd;
+							 list.remove(index+1);
+						 }
+					 }
+					 return list;
+				}			
+				
+				
+				if(e.columnEnd<cur.columnStart){
+					list.add(0,e);
+					return list;
+			    }
+					
+				if(e.columnStart>cur.columnEnd){
+					if(index==list.size()-1){
+						list.add(e);
+						return list;
+					}
+				}
+				index++;
+			}
+		}
+		return list;		
+		
 	}
 	
 	public static void main(String[] args) {
@@ -53,7 +127,7 @@ public class GridlandMetro {
 				int trainColumnEnd= scanner.nextInt();
 				Track curTrack =new Track(trainRow,trainColumnStart,trainColumnEnd);
 				if(trackMap.containsKey(trainRow)){
-					trackMap.get(trainRow).add(curTrack);
+					trackMap.put(trainRow,getTrackList(trackMap.get(trainRow),curTrack));
 				}else{
 					List<Track> trackList = new ArrayList<Track>();
 					trackList.add(curTrack);
